bdf402eaba61c026662abbb2698bdca922c7ff53
Vincent Roquelaure <roquelaurevincent@gmail.com>
2023-01-08 01:02:50 +0100

    Fix Gradle - Added libs folder

--- install-dependency.sh
+++ install-dependency.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ -d ./libs ]]; then
+  echo "remove existing libs directory"
+  rm -r ./libs
+fi
+
+mkdir ./libs && cd ./libs || exit
+
 download() {
 	html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=$1"`
 	curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=$1" -o $2
