4cd9d6cd05c3f859b8486969957036a1ebff8075
Noltox <75265787+NoltoxGit@users.noreply.github.com>
2023-06-10 21:42:54 +0200

    [⬆️] Upgrade com.github.johnrengelman.shadow

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,7 +2,7 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
 }
 
