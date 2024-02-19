5159c069eb3ec1f28150da036e5a1fd3323a6651
Vincent Roquelaure <roquelaurevincent@gmail.com>
2024-02-20 00:50:28 +0100

    Fixed message interpretation in click order

--- core/src/main/java/fr/aerwyn81/headblocks/events/OnPlayerInteractEvent.java
+++ core/src/main/java/fr/aerwyn81/headblocks/events/OnPlayerInteractEvent.java
@@ -121,8 +121,9 @@ public class OnPlayerInteractEvent implements Listener {
                 if (HeadService.getChargedHeadLocations().stream()
                         .filter(h -> h.getUuid() != headLocation.getUuid() && !playerHeads.contains(h.getUuid()))
                         .anyMatch(h -> h.getOrderIndex() <= headLocation.getOrderIndex())) {
-                    player.sendMessage(LanguageService.getMessage("Messages.OrderClickError")
-                            .replaceAll("%name%", headLocation.getDisplayedName()));
+                    player.sendMessage(PlaceholdersService.parse(player.getName(), player.getUniqueId(),
+                            LanguageService.getMessage("Messages.OrderClickError")
+                                    .replaceAll("%name%", headLocation.getDisplayedName())));
                     return;
                 }
             }
