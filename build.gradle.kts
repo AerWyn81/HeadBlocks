5b90a0158b153934db2ee2e1f8e34db2685950ef
Vincent Roquelaure <roquelaurevincent@gmail.com>
2024-10-20 12:47:46 +0200

    Cleanup project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,3 @@
 plugins {
     id("java")
 }
-
-version = "2.6.7"
-
-allprojects {
-    repositories {
-        mavenCentral()
-
-        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        maven("https://repo.codemc.org/repository/maven-public")
-        maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-        maven("https://repo.fancyplugins.de/releases")
-        maven("https://jitpack.io")
-    }
-}
--- core/src/main/java/fr/aerwyn81/headblocks/data/head/HeadType.java
+++
@@ -1,5 +0,0 @@
-package fr.aerwyn81.headblocks.data.head;
-
-public enum HeadType {
-    DEFAULT, PLAYER, HDB
-}
