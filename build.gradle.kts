6554f303bdd6cddba1647243804037d9110922cf
Vincent Roquelaure <roquelaurevincent@gmail.com>
2026-03-08 21:13:59 +0100

    Added Hangar auto publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import io.papermc.hangarpublishplugin.model.Platforms
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
@@ -6,6 +7,7 @@ plugins {
     alias(libs.plugins.shadow)
     alias(libs.plugins.plugin.yml)
     alias(libs.plugins.run.paper)
+    alias(libs.plugins.hangar.publish)
 }
 
 version = "3.0.0"
@@ -210,6 +212,38 @@ bukkit {
         register("headblocks.admin") {
             description = "Allows access to /headblocks admin commands"
             default = BukkitPluginDescription.Permission.Default.OP
+        }
+    }
+}
+
+hangarPublish {
+    publications.register("plugin") {
+        version = project.version as String
+        id = "HeadBlocks"
+        channel = "Release"
+        changelog = System.getenv("HANGAR_CHANGELOG") ?: ""
+        apiKey = System.getenv("HANGAR_API_TOKEN") ?: ""
+
+        platforms {
+            register(Platforms.PAPER) {
+                jar = tasks.shadowJar.flatMap { it.archiveFile }
+                platformVersions = listOf(
+                    "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6",
+                    "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6",
+                    "1.21.7", "1.21.8", "1.21.9", "1.21.10"
+                )
+                dependencies {
+                    hangar("PlaceholderAPI") {
+                        required = false
+                    }
+                    hangar("HeadDatabase") {
+                        required = false
+                    }
+                    hangar("packetevents") {
+                        required = false
+                    }
+                }
+            }
         }
     }
 }
